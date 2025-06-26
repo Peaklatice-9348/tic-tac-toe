17ed0d77d0ac7b2ce6330d0d0c7a0e45a8f744ab
Peaklatice-9348 <yusufjavaid.072012@gmail.com>
2025-06-26 19:48:28 +0100

    Add files via upload

--- Tic tac toe/main.py
+++ Tic tac toe/main.py
@@ -7,15 +7,45 @@ window.title('tic tac toe')
 buttons = []
 turn='x'
 
+def restart():
+    for i in range(3):
+        for j in range(3):
+            buttons[i][j].config(state=DISABLED)
+
+def check_winner():
+    for i in range(3):
+
+        if buttons[i][0]['text'] == buttons[i][1]['text'] == buttons[i][2]['text'] != '':
+            return buttons[i][0]['text']
+        
+        if buttons[0][i]['text'] == buttons[1][i]['text'] == buttons[2][i]['text'] != '':
+            return buttons[0][i]['text']
+        
+    if buttons[0][0]['text'] == buttons[1][1]['text'] == buttons[2][2]['text'] != '':
+        return buttons[0][0]['text']
+    
+    if buttons[0][2]['text'] == buttons[1][1]['text'] == buttons[2][0]['text'] != '':
+        return buttons[1][1]['text']
+
+    return None
+
 def box(row,column):
     global turn
     if buttons[row][column]['text'] == '':
         buttons[row][column].config(text=turn)
-        if turn == 'x':
-            turn = 'o'
+        winner = check_winner()
+        if winner:
+            label.config(text=f'player {winner} wins!')
+            restart()
+        elif all(all(cell['text'] != '' for cell in row)for row in buttons):
+            label.config(text='Draw!')
+            restart()
         else:
-            turn='x'
-        label.configure(text=f"Player {turn}'s turn")
+            if turn == 'x':
+                turn = 'o'
+            else:
+                turn='x'
+            label.configure(text=f"Player {turn}'s turn")
 
 for i in range(3):
     row=[]
